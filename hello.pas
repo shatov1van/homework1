2d6e130ce5a1a94d8f474ff168743331373f2b51
Иван Шатов <ivanshatov13@gmail.com>
2024-10-13 19:05:28 +0300

    Add hello fail

---
+++ hello.pas
@@ -0,0 +1,13 @@
+﻿//Измените код в файле hello.pas, чтобы программа спрашивала у пользователя язык, на котором он говорит и выводила приветствие на этом языке.
+begin
+  Println('На каком языке вы говорите? Если на русском напишите 1.');
+  Println('What language do you speak? If in English, then write 2.');
+  Println('Сіз қай тілде сөйлейсіз? Егер қазақ тілінде болса, 3 жазыңыз.');
+  var N := ReadInteger();
+  Assert(N in 1..3);
+  case N of
+    1: Println('Привет, дорогой друг.');
+    2: Println('Hello, my dear friend.');
+    3: Println('Сәлем, қымбатты досым.');
+  end;
+end.
